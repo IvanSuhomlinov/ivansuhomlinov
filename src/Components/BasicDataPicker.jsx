d6bf352142a62f2be40d723b4040232d97f48637
Ivan Suhomlinov <isuhomlinov57@gmail.com>
2025-06-24 19:00:59 +0500

    fixed

--- src/Components/BasicDataPicker.jsx
+++ src/Components/BasicDataPicker.jsx
@@ -10,7 +10,7 @@ const BasicDatePicker = ({getCurDate, currentDate, ...props}) => {
             style={{ width: "max-content"}}
             label="Введите дату"
             format="DD.MM.YYYY"
-            /*defaultValue={dayjs(Date.now())}*sss/
+            /*defaultValue={dayjs(Date.now())}*/
             onChange={getCurDate}
             value={currentDate}
           />
